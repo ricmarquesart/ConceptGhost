a8ed433f273b9187bc7f00f8e5fd786ef1389160
ricmarquesart <ricmarquesart@gmail.com>
2026-09-18 23:28:44 -0700

    release: freeze v0.32 source snapshot and bundle authority

--- releases/ConceptGhost_v0.17.2_HeroMesh_Camera_FBX_UV/INSTALL_AFTER_RESTORE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableExtensions
-set "HERE=%~dp0"
-if not exist "%HERE%ConceptGhost_v0.17.2_HeroMesh_Camera_FBX_UV\INSTALL_CONCEPTGHOST_V0.17.2.bat" (
-  echo Run RESTORE_BUNDLE.bat first.
-  exit /b 1
-)
-call "%HERE%ConceptGhost_v0.17.2_HeroMesh_Camera_FBX_UV\INSTALL_CONCEPTGHOST_V0.17.2.bat"
-exit /b %ERRORLEVEL%
--- scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-"""ConceptGhost bootstrap package."""
