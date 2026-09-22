462bb02b7b291c582b6a4f141c0b3cd803d2e4fe
ricmarquesart <ricardomarques3d@gmail.com>
2026-09-21 20:08:59 -0700

    preview: version Gate 2 diagnostics r2

--- Experimental/P10_Lab/p10_lab/__init__.py
+++ Experimental/P10_Lab/p10_lab/__init__.py
@@ -2,6 +2,6 @@
 
 from .preview_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__version__ = "0.2.0-gate2-preview"
+__version__ = "0.2.1-gate2-preview-r2"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
