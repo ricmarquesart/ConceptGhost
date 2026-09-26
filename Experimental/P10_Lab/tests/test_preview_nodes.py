5fcb77b44f53b9391c9601d73d0c1fef79197948
ricmarquesart <ricardomarques3d@gmail.com>
2026-09-25 22:23:27 -0700

    test: require CG-02 and CG-03 panorama nodes in package

--- Experimental/P10_Lab/tests/test_preview_nodes.py
+++ Experimental/P10_Lab/tests/test_preview_nodes.py
@@ -99,6 +99,8 @@ class Gate2PreviewNodeTests(unittest.TestCase):
         self.assertIn("ConceptGhostP10CompletionBundleBuilder", p10_lab.NODE_CLASS_MAPPINGS)
         self.assertIn("ConceptGhostP10BundleLoader", p10_lab.NODE_CLASS_MAPPINGS)
         self.assertIn("ConceptGhostP10SourceConceptImage", p10_lab.NODE_CLASS_MAPPINGS)
+        self.assertIn("ConceptGhostP10PanoramaResultPublisher", p10_lab.NODE_CLASS_MAPPINGS)
+        self.assertIn("ConceptGhostP10PanoramaValidation", p10_lab.NODE_CLASS_MAPPINGS)
         self.assertEqual(
             p10_lab.NODE_DISPLAY_NAME_MAPPINGS["ConceptGhostP10BundleLoader"],
             "P10 P9 Bundle Loader / Validator",
